df0806cdba7828590bff02bf088730f9f7ae5fb0
Bret Comnes <bcomnes@gmail.com>
2022-01-21 21:29:12 -0800

    Add basic tree printing

--- examples/basic/src/conflict-page/page.js
+++
@@ -1,9 +0,0 @@
-import { html, render } from 'uhtml-ssr'
-
-export default async function conflictPage () {
-  return render(String, html`
-    <div class='blue-text'>
-      conflicting page
-    </div>
-`)
-}
