4247e7fa4e34abeda2a12f24df0102ad1ce18e5e
Bret Comnes <bcomnes@gmail.com>
2024-06-21 13:41:37 -0700

    Update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
-import neostandard from 'neostandard'
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
 
 export default neostandard({
   ignores: [
+    ...resolveIgnoresFromGitignore(),
     'test-cases/build-errors/src/**/*.js',
     'test-cases/page-build-errors/src/**/*.js',
   ],
