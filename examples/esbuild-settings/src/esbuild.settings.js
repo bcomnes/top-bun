e1cea35706c95a98fd7ae262fdc2c42893495603
Bret Comnes <bcomnes@gmail.com>
2024-10-23 12:51:35 -0700

    Add type example

--- examples/esbuild-settings/src/esbuild.settings.js
+++ examples/esbuild-settings/src/esbuild.settings.js
@@ -1,5 +1,10 @@
+/** @import { BuildOptions } from 'esbuild' */
 import { polyfillNode } from 'esbuild-plugin-polyfill-node'
 
+/**
+ * @param  {BuildOptions} esbuildSettings
+ * @return {Promise<BuildOptions>}
+ */
 export default async function esbuildSettingsOverride (esbuildSettings) {
   esbuildSettings.plugins = [
     polyfillNode(),
