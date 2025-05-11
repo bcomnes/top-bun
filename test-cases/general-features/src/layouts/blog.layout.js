f1e30dd5feebf2f4a6bd3dbe70d629bc9f27e05b
Bret Comnes <bcomnes@gmail.com>
2025-05-11 12:50:45 -0700

    Switch default layout and test to use preact

--- test-cases/general-features/src/layouts/blog.layout.js
+++ test-cases/general-features/src/layouts/blog.layout.js
@@ -1,6 +1,5 @@
 import defaultRootLayout from './root.layout.js'
-// @ts-ignore
-import { html } from 'uhtml-isomorphic'
+import { html } from 'htm/preact'
 
 /**
  * @template {Record<string, any>} T
@@ -17,18 +16,18 @@ export default function blogLayout (layoutVars) {
   const vars = layoutVars.vars
 
   const children = html`
-    <article class="article-layout h-entry" itemscope itemtype="http://schema.org/NewsArticle">
-      <header class="article-header">
-        <h1 class="p-name article-title" itemprop="headline">${vars.title}</h1>
-        <div class="metadata">
-          <address class="author-info" itemprop="author" itemscope itemtype="http://schema.org/Person">
+    <article className="article-layout h-entry" itemscope itemtype="http://schema.org/NewsArticle">
+      <header className="article-header">
+        <h1 className="p-name article-title" itemprop="headline">${vars.title}</h1>
+        <div className="metadata">
+          <address className="author-info" itemprop="author" itemscope itemtype="http://schema.org/Person">
             ${vars.authorImgUrl
-              ? html`<img height="40" width="40"  src="${vars.authorImgUrl}" alt="${vars.authorImgAlt}" class="u-photo" itemprop="image">`
+              ? html`<img height="40" width="40" src="${vars.authorImgUrl}" alt="${vars.authorImgAlt}" className="u-photo" itemprop="image" />`
               : null
             }
             ${vars.authorName && vars.authorUrl
               ? html`
-                  <a href="${vars.authorUrl}" class="p-author h-card" itemprop="url">
+                  <a href="${vars.authorUrl}" className="p-author h-card" itemprop="url">
                     <span itemprop="name">${vars.authorName}</span>
                   </a>`
               : null
@@ -36,35 +35,40 @@ export default function blogLayout (layoutVars) {
           </address>
           ${vars.publishDate
             ? html`
-              <time class="dt-published" itemprop="datePublished" datetime="${vars.publishDate}">
-                <a href="#" class="u-url">
+              <time className="dt-published" itemprop="datePublished" datetime="${vars.publishDate}">
+                <a href="#" className="u-url">
                   ${(new Date(vars.publishDate)).toLocaleString()}
                 </a>
               </time>`
             : null
           }
           ${vars.updatedDate
-            ? html`<time class="dt-updated" itemprop="dateModified" datetime="${vars.updatedDate}">Updated ${(new Date(vars.updatedDate)).toLocaleString()}</time>`
+            ? html`
+                <time className="dt-updated" itemprop="dateModified" datetime="${vars.updatedDate}">
+                  Updated ${(new Date(vars.updatedDate)).toLocaleString()}
+                </time>`
             : null
           }
         </div>
       </header>
 
-      <section class="e-content" itemprop="articleBody">
+      <section className="e-content" itemprop="articleBody">
         ${typeof innerChildren === 'string'
-          ? html([innerChildren])
-          : innerChildren /* Support both uhtml and string children. Optional. */
+          ? html`<div dangerouslySetInnerHTML="${{ __html: innerChildren }}" />`
+          : innerChildren
         }
       </section>
 
       <!--
         <footer>
-            <p>Footer notes or related info here...</p>
+          <p>Footer notes or related info here...</p>
         </footer>
       -->
     </article>
   `
 
-  const rootArgs = { ...rest, children }
-  return defaultRootLayout(rootArgs)
+  return defaultRootLayout({
+    ...rest,
+    children
+  })
 }
--- test-cases/general-features/src/layouts/root.layout.js
+++ test-cases/general-features/src/layouts/root.layout.js
@@ -1,5 +1,5 @@
-// @ts-ignore
-import { html, render } from 'uhtml-isomorphic'
+import { html } from 'htm/preact'
+import { render } from 'preact-render-to-string'
 
 /**
  * @template {Record<string, any>} T
@@ -8,14 +8,14 @@ import { html, render } from 'uhtml-isomorphic'
 
 /**
  * @typedef {{
- * title: string
- * siteName: string,
- * authorImgUrl: string
- * authorName: string,
- * authorUrl: string,
- * authorImgAlt: string,
- * publishDate: string,
- * updatedDate?: string
+ *   title: string,
+ *   siteName: string,
+ *   authorImgUrl: string,
+ *   authorName: string,
+ *   authorUrl: string,
+ *   authorImgAlt: string,
+ *   publishDate: string,
+ *   updatedDate?: string
  * }} SiteVars
  */
 
@@ -23,34 +23,46 @@ import { html, render } from 'uhtml-isomorphic'
 export default function defaultRootLayout ({
   vars: {
     title,
-    siteName = 'TopBun',
+    siteName = 'TopBun'
   },
   scripts,
   styles,
-  children,
-  /* pages */
+  children
 }) {
-  return render(String, html`
-    <!DOCTYPE html>
-    <html>
+  const pageTitle = title && siteName
+    ? `${title} | ${siteName}`
+    : title || siteName
+
+  const head = render(html`
     <head>
-      <meta charset="utf-8">
-      <title>${title ? `${title}` : ''}${title && siteName ? ' | ' : ''}${siteName}</title>
+      <meta charset="utf-8" />
+      <title>${pageTitle}</title>
       <meta name="viewport" content="width=device-width, user-scalable=no" />
-      <meta itemprop="publisher" content="${siteName}">
-      <meta property="og:site_name" content="${siteName}">
-      ${scripts
-        ? scripts.map(script => html`<script type='module' src="${script}"></script>`)
-        : null}
-      ${styles
-        ? styles.map(style => html`<link rel="stylesheet" href=${style} />`)
-        : null}
+      <meta itemprop="publisher" content="${siteName}" />
+      <meta property="og:site_name" content="${siteName}" />
+      ${scripts?.map(script =>
+        html`<script type="module" src="${script}" />`
+      )}
+      ${styles?.map(style =>
+        html`<link rel="stylesheet" href="${style}" />`
+      )}
     </head>
-    <body class="safe-area-inset">
-      <main class="mine-layout">
-        ${typeof children === 'string' ? html([children]) : children /* Support both uhtml and string children. Optional. */}
+  `)
+
+  const body = render(html`
+    <body className="safe-area-inset">
+      <main className="mine-layout">
+        ${typeof children === 'string'
+          ? html`<div dangerouslySetInnerHTML="${{ __html: children }}" />`
+          : children
+        }
       </main>
     </body>
-    </html>
-`)
+  `)
+
+  return `<!DOCTYPE html>
+<html>
+  ${head}
+  ${body}
+</html>`
 }
